3e129fa300b05959e80c31190eed0160f4a7efba
damian abalos <damianabalos@hotmail.com>
2021-09-22 21:57:05 -0300

    cree el NavBar para el segundo desafío

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import './components/NavBar/NavBar.css'
 import './App.css';
+// import DynamicObjects from "./components/DynamicObjects"
+// import SpreadOperators from "./components/SpreadOperator/SpreadOperators";
+import "./components/NavBar/NavBar";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Lo logre :')
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar/>
     </div>
   );
 }
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const NavBar = () => {
+
+    // let apagado = true
+    // let blackTheme = apagado ? 
+
+
+
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li><a href="#home">Home</a></li>                        
+                    <li><a href="#secciones">Secciones</a></li>                        
+                    <li><a href="#nosotros">Nosotros</a></li>                        
+                    <li><a href="#contacto">Contacto</a></li>                        
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+
+
+export default NavBar;
