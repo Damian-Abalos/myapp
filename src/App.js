02922357c4aa8b000c858c98c7428d90560517f3
damian abalos <damianabalos@hotmail.com>
2021-09-25 19:24:03 -0300

    instale fontawesome

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 // import SpreadOperators from "./components/SpreadOperator/SpreadOperators";
 import "./components/NavBar/NavBar";
 import NavBar from "./components/NavBar/NavBar";
+import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <button className="btn btn-primary"></button>
     </div>
   );
 }
