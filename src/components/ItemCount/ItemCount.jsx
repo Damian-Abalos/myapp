17eb189cd05e77e5564f582713939bf319bdd723
damian abalos <damianabalos@hotmail.com>
2021-11-02 22:02:57 -0300

    no me anda el disabled en el agregar al carrito

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -5,49 +5,80 @@ import { useCartContext } from "../../context/cartContext";
 import { higherThanStock } from "../../helpers";
 import "./ItemCount.css";
 
-function ItemCount({id, stock, initial, nombre, img, precio}) {
-
-  const {agregarItem} = useCartContext()
+function ItemCount({ id, stock, initial, nombre, img, precio }) {
+  const { agregarItem } = useCartContext();
 
   const [qty, setQty] = useState(initial);
 
   const addOne = () => {
-    if (!higherThanStock(qty,stock)) setQty(qty + 1);
+    if (!higherThanStock(qty, stock)) setQty(qty + 1);
   };
 
   const removeOne = () => {
     if (higherThanStock(qty, 1)) setQty(qty - 1);
   };
-  
+
   const onAdd = (qty) => {
-    agregarItem({item:nombre , cantidad:qty , img:img, price:precio, stock:stock, id:id})
-    apagarBoton()
-  }
-  
-  const [botonActivo, setBotonActivo] = useState(false) 
+    agregarItem({
+      item: nombre,
+      cantidad: qty,
+      img: img,
+      price: precio,
+      stock: stock,
+      id: id,
+    });
+    apagarBoton();
+  };
+
+  const [botonActivo, setBotonActivo] = useState(false);
 
   function apagarBoton() {
-    setBotonActivo(true)
+    setBotonActivo(true);
   }
+
+  // const [habilitarBoton, setHabilitarBoton] = useState();
+  // {!qty ? setHabilitarBoton("false") : setHabilitarBoton("true")}
 
   return (
     <div>
       {!botonActivo ? (
         <div>
-        <div id="ItemCount">
-          <button className="btn btn-danger btn-remove" onClick={() => removeOne()}>-</button>
-          <p className="text-dark">{qty}</p>
-          <button className="btn btn-primary btn-add text-center" onClick={() => {addOne()}}>+</button>
+          <div id="ItemCount">
+            <button
+              className="btn btn-danger btn-remove"
+              onClick={() => removeOne()}
+            >
+              -
+            </button>
+            <p className="text-dark">{qty}</p>
+            <button
+              className="btn btn-primary btn-add text-center"
+              onClick={() => {
+                addOne();
+              }}
+            >
+              +
+            </button>
+          </div>
+          <div>
+            <button
+              // disabled={habilitarBoton}
+              className="btn btn-primary btnAddCart"
+              onClick={() => onAdd(qty)}
+            >
+              Añadir al carrito
+            </button>
+          </div>
         </div>
-        <div>
-          <button className="btn btn-primary btnAddCart" onClick={() => onAdd(qty)}>Añadir al carrito</button>
-        </div>
-      </div>
       ) : (
-      <div className="text-center">
-        <Link to="/cart" className="btn btn-success m-3">Terminar mi Compra</Link>
-        <Link to="/" className="btn btn-primary m-3">Seguir comprando</Link>
-      </div> 
+        <div className="text-center">
+          <Link to="/cart" className="btn btn-success m-3">
+            Terminar mi Compra
+          </Link>
+          <Link to="/" className="btn btn-primary m-3">
+            Seguir comprando
+          </Link>
+        </div>
       )}
     </div>
   );
